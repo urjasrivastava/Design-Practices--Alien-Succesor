f0808b56613820d72353d41d6cf3d7f04a967a8f
urja <urjasrivastava123@gmail.com>
2021-08-18 10:47:57 +0530

    removing side effects on the input string, a new string is created to store the successor

--- main.cpp
+++ main.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-void succ_alien(string &n, string B)
+string succ_alien(string n, string B)
 {
     int base = B.size() - 1, j = 0;
     int len = n.size(), i = len - 1;
-    if (len == 0 or base == 0)
-        return;
-    
-    map<char,int> alien_to_decimal;
-    for(char c:B)
+    string ans = "";
+    if (len == 0 or base <= 0)
+        return ans;
+
+    map<char, int> alien_to_decimal;
+    for (char c : B)
     {
-        alien_to_decimal[c]=j++;
+        alien_to_decimal[c] = j++;
     }
 
     // increment from the right
     while (i >= 0 && n[i] == B[base])
     {
-        n[i] = B[0];  //carry to the next position
+        ans += B[0]; //carry to the next position
         i--;
     }
     if (i < 0)
     {
-        n = B[1] + n;
+        ans = B[1] + ans;
     }
     else
     {
-        int index = alien_to_decimal[n[i]];        
-        n[i] = B[index + 1];
+        int index = alien_to_decimal[n[i]];
+        ans = n.substr(0, i) + B[index + 1] + ans;
     }
+    return ans;
 }
 int main()
 {
     string n = "!@^&*";
     string b = "!@^&*";
     cout << n << endl;
-    succ_alien(n, b);
-    cout << n << endl;
+    cout << succ_alien(n, b) << endl;
 }
